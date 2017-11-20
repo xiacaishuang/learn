d4e9e5ae7f86407135a5431863734db27654bdb4
xiacaishuang <751405162@qq.com>
2017-11-20 19:09:47 +0800

    mybook

---
+++ my_book/mybook/mybook/urls.py
@@ -0,0 +1,53 @@
+"""mybook URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from account import views
+from django.contrib.auth.views import logout,login
+from django.contrib.auth.views import logout_then_login
+from django.contrib.auth.views import password_change
+from django.contrib.auth.views import password_change_done
+from django.contrib.auth.views import password_reset
+from django.contrib.auth.views import password_reset_done
+from django.contrib.auth.views import password_reset_confirm
+from django.contrib.auth.views import password_reset_complete
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    #url(r'^login/$',views.user_login,name='login'),
+    url(r'^login/$',login,name='login'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
+    # change password
+    url(r'^password-change/$', password_change, name='password_change'),
+    url(r'^password-change/done/$', password_change_done, name='password_change_done'),
+    # reset password
+    ## restore password urls
+    url(r'^password-reset/$',
+        password_reset,
+        name='password_reset'),
+    url(r'^password-reset/done/$',
+        password_reset_done,
+        name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^password-reset/complete/$',
+        password_reset_complete,
+        name='password_reset_complete'),
+    url(r'^$', views.dashboard, name='dashboard'),
+
+]
