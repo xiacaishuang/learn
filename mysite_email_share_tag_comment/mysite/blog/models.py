4f8ae264aa316c067a7a24d418bda2c27d2b9e6a
xiacaishuang <751405162@qq.com>
2017-11-20 13:56:21 +0800

    mysite_email_share_tag_comment

---
+++ mysite_email_share_tag_comment/mysite/blog/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
+# Create your models here.
+class Post(models.Model):
+    """docstring for Post.models.Modelef __init__(self, arg):
+        super(Post,models.Model.__init__()
+        self.arg = arg"""
+    STATUS_CHOICES = (
+        ('draft','Draft'),
+        ('published','Published')
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250,unique_for_date='publish')
+    author = models.ForeignKey(User,related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+                                choices=STATUS_CHOICES,
+                                default='draft')
+    class Meta:
+        ordering = ('-publish',)
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse('post_detail',#按照view中name为post_detail的url生成url地址
+                        args=[self.publish.year,
+                              self.publish.strftime('%m'),
+                              self.publish.strftime('%d'),
+                              self.slug])
+    tags = TaggableManager()
+class Comment(models.Model):
+    post = models.ForeignKey(Post,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name,self.post)
