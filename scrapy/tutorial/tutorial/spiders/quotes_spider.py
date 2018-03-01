063ef0290b0bc9068138aab4ebeb0451fe78a9f4
xiacaishuag <751405162@qq.com>
2018-03-01 17:20:34 +0800

    new check

--- scrapy/tutorial/tutorial/spiders/quotes_spider.py
+++ scrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -21,30 +21,20 @@ import scrapy
 '''
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-
-    def start_requests(self):
-        baseurl = 'http://www.cctv-axsq.cn/a/xinwendongtai/list_18_'
-        urls = []
-        for i in range(1,20):
-            urls.append(baseurl+str(i)+'.html')
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    '''def parse(self, response):
-        page = response.url[-6]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)'''
-
+    start_urls = ['http://www.cctv-axsq.cn/a/xinwendongtai/',]
     def parse(self, response):
-        #page = response.url[-6]
         filename = 'content.txt'
         with open(filename, 'a') as f:
-            #f.write(response.body)
             for i in range(len(response.css("div.new_content"))):
-                title='Title:'+response.css("p.new_title")[i].css("p::text").extract_first()+'\n'
-                content='Content:'+response.css("div.new_content")[i].css("div::text").extract_first()+'\n\n'
+                title = 'Title:' + response.css("p.new_title")[i].css("p::text").extract_first() + '\n'
+                content = 'Content:' + response.css("div.new_content")[i].css("div::text").extract_first() + '\n\n'
                 f.write(title)
                 f.write(content)
+        if('下一页' in response.css('div.pages a::text').extract()):
+            next_page = response.css('div.pages a::attr(href)')[-2].extract()
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+
+
+
 
